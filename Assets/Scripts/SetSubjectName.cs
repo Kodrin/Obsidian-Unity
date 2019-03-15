5b45b1f14d4cf85f7f749318440888dac3f3d1ae
Codrin Tablan Negrei <kodrintxps@gmail.com>
2019-03-14 22:55:37 -0400

    created script that set the textmesh text to the parent

---
+++ Assets/Scripts/SetSubjectName.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSubjectName : MonoBehaviour
+{
+	//component 
+	TextMesh _txtMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	//fetch the text mesh
+    	_txtMesh = gameObject.GetComponent<TextMesh>();
+		//fetch the parent
+    	_txtMesh.text = gameObject.transform.parent.name;
+    	//debug
+    	// Debug.Log(gameObject.transform.parent.name);
+    }
+
+}
