493edeb69589260b06da4160b24a9e68afe8d4b2
Codrin Tablan Negrei <kodrintxps@gmail.com>
2019-03-17 23:42:58 -0400

    Idle/Tracking/Initiation animation on video canvas with kinect

--- Assets/Scripts/VideoOnTerminal.cs
+++ Assets/Scripts/VideoOnTerminal.cs
@@ -6,6 +6,18 @@ using UnityEngine.Video;
 
 public class VideoOnTerminal : MonoBehaviour {
     
+    //skeleton reference
+    public PlayerController _skeleton;
+    public float _initThreshold = 2.0f;
+    public float _handTimer = 0;
+    private bool _hasChangedVideo = false;
+
+    //list of video clips
+    public VideoClip _idle;
+    public VideoClip _tracking;
+    public VideoClip _initializing;
+
+    //Video variable references
     public RawImage rawImage;
     public VideoPlayer videoPlayer;
     public AudioSource audioSource;
@@ -13,8 +25,12 @@ public class VideoOnTerminal : MonoBehaviour {
     // Use this for initialization
     void Start () {
         StartCoroutine(PlayVideo());
+
     }
-  
+
+    void Update () {
+        InitializeObsidian();
+    } 
     
     IEnumerator PlayVideo()
     {
@@ -25,8 +41,51 @@ public class VideoOnTerminal : MonoBehaviour {
             yield return waitForSeconds;
             break;
         }
-        rawImage.texture = videoPlayer.texture;
+        // rawImage.texture = videoPlayer.texture;
         videoPlayer.Play();
         audioSource.Play();
      }
+
+    //timer that waits for the video to finish 
+    IEnumerator ResetChangeVideo(float lengthOfVideo)
+    {
+        yield return new WaitForSeconds(lengthOfVideo);
+        _hasChangedVideo = false;
+     }
+
+    //init obsidian animation transition
+    public void ChangeVideo(VideoClip nextVideo)
+    {
+        videoPlayer.clip = nextVideo;
+        videoPlayer.Play();
+        audioSource.Play();
+
+        //has changed video
+        _hasChangedVideo = true;
+        
+        //Wait for the video to finish before starting the next
+        StartCoroutine(ResetChangeVideo((float)nextVideo.length));
+
+     }
+
+    //Handles the idle/tracking/initiate of the experience
+    public void InitializeObsidian(){
+        //if your body is not tracked, then play the idle anima
+        if(!BodySourceView.bodyTracked && !_hasChangedVideo)
+            ChangeVideo(_idle);
+
+        //plays the initializes anim after the partipant holds his hands together for 5 seconds
+        if(_skeleton._handDistance < _initThreshold && BodySourceView.bodyTracked == true){
+            //increase the timer
+            _handTimer += Time.deltaTime;
+
+            if(_handTimer > 5.0f && !_hasChangedVideo){
+                ChangeVideo(_initializing);
+                
+            }
+
+        } else if(BodySourceView.bodyTracked && !_hasChangedVideo){
+            ChangeVideo(_tracking);
+        }
+    }
 }
