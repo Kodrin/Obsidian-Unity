94e1e2b794ff008af03532feb7faa5e4127abe37
Codrin Tablan Negrei <kodrintxps@gmail.com>
2019-03-17 23:42:26 -0400

    Tracking the distance between hands

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,15 @@
-﻿//-----------------------------------------
-//   PlayerController.cs
-//
-//   Jason Walters
-//   http://glitchbeam.com
-//   @jasonrwalters
-//
-//   edit 5/25/2015
-//-----------------------------------------
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    public float boundsX;
-    public float speed;
-    public float tilt;
-
-    private Rigidbody rb;
-    private float inputHorizontal;
+    public bool _isDebugging = false;
+    public float _handDistance = 15.0f;
+    private float _inputHorizontal;
 
     void Start()
     {
-        // reference components
-        rb = GetComponent<Rigidbody>();
+
     }
 
     void Update()
@@ -34,45 +20,34 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // run movement function to handle rigidbody physics
-        Movement();
+
     }
 
     void PlayerInput()
     {
         if (BodySourceView.bodyTracked)
-        {
+        {   
+
             // fetch hand positions
             Vector3 handLeft = BodySourceView.jointObjs[7].position;
             Vector3 handRight = BodySourceView.jointObjs[11].position;
+
+            //calculate hand distance
+            _handDistance = Vector3.Distance(handLeft,handRight);
 
             // calc angle of hands
             float angle = Mathf.Atan2(handRight.y - handLeft.y, handRight.x - handLeft.x) * Mathf.Rad2Deg;
 
             // convert angle rotation to movement values
-            inputHorizontal = Mathf.Lerp(1.0f, -1.0f, Mathf.InverseLerp(-45.0f, 45.0f, angle));
-        }
-        else
-        {
-            // fetch our input for movememnt
-            inputHorizontal = Input.GetAxis("Horizontal");
+            _inputHorizontal = Mathf.Lerp(1.0f, -1.0f, Mathf.InverseLerp(-45.0f, 45.0f, angle));
+
+            //debug
+            if(_isDebugging){
+                Debug.Log("hand distance" + _handDistance);
+                Debug.Log("Angle" + angle);
+                Debug.Log("Input Horizontal" + _inputHorizontal);
+            }
         }
     }
 
-    void Movement()
-    {
-        // update player velocity
-        Vector3 input = new Vector3(inputHorizontal, 0.0f, 0.0f);
-
-        // update player velocity
-        rb.velocity = input * speed;
-
-        // apply hover effect, and clamp player within bounds
-        Vector3 pos = rb.position;
-        rb.position = new Vector3(Mathf.Clamp(pos.x, -boundsX, boundsX), pos.y, pos.z);
-
-        // apply tilt effect to our rotation
-        float tiltZ = rb.velocity.x * -tilt;
-        rb.rotation = Quaternion.Euler(0.0f, 0.0f, tiltZ);
-    }
 }
