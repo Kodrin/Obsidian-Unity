5b5eba9254defbd1456b80beebf92f4cfa676a95
Codrin Tablan Negrei <kodrintxps@gmail.com>
2019-03-17 20:35:14 -0400

    Imported Animations form Val and Charle's video script

---
+++ Assets/Scripts/VideoOnTerminal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoOnTerminal : MonoBehaviour {
+    
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(PlayVideo());
+    }
+  
+    
+    IEnumerator PlayVideo()
+    {
+        videoPlayer.Prepare();
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
+     }
+}
